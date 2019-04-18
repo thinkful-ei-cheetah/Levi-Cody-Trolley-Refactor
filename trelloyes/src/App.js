ffbf1dda75ac20cbf2d79f3eb3444fc3b3524f0d
Cody Gillette <cgillette1312@gmail.com>
2019-04-18 09:48:20 -0700

    updated

--- trelloyes/src/App.js
+++ trelloyes/src/App.js
@@ -3,26 +3,35 @@ import List from './List'
 import './App.css';
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
-  };
+constructor(props){
+  super()
+  this.state = props
+  }
+   
+  
+
+  handleDelete = (id) => {
+    console.log(this.state)
+    this.setState({
+      store : this.state.store.lists.filter((item,index ) => index !== id)
+    })
+  }
 
   render() {
-    const { store } = this.props
+    // const { store } = this.state
     return (
       <main className='App'>
         <header className='App-header'>
           <h1>Trelloyes!</h1>
         </header>
+        {console.log(this.state)}
         <div className='App-list'>
-          {store.lists.map(list => (
+          {state.store.lists.map(list => (
             <List
               key={list.id}
               header={list.header}
               cards={list.cardIds.map(id => store.allCards[id])}
+              handleDelete={()=>this.handleDelete()}
             />
           ))}
         </div>
